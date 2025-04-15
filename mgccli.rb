70bfca00d97c1905124eb647ddbb15e941e130f4
MagaluCloud <magalucloud.cli@luizalabs.com>
2025-04-15 12:29:56 +0000

    Brew formula update for mgccli version v0.38.0

--- mgccli.rb
+++ mgccli.rb
@@ -11,7 +11,7 @@ class Mgccli < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/MagaluCloud/mgccli/releases/download/v0.38.0/mgccli_0.38.0_darwin_amd64.tar.gz"
-      sha256 "36c4e48723b840beaff19c143784f7736018d39f0593d08f7233293c13379882"
+      sha256 "b31b15bbbd9a9dc8274cb1f913d4809a8c7b95ae42187cd1b4dbcb9a445720e0"
 
       def install
         bin.install "mgc"
@@ -19,7 +19,7 @@ class Mgccli < Formula
     end
     if Hardware::CPU.arm?
       url "https://github.com/MagaluCloud/mgccli/releases/download/v0.38.0/mgccli_0.38.0_darwin_arm64.tar.gz"
-      sha256 "e91986d78bc2059efe541ebd350dad82f47add916f12552fd724aa6990e2b5b7"
+      sha256 "1ec6f62fcaed92c1900c77d0e965938ce0b3694c3a94875f716225699e74caf5"
 
       def install
         bin.install "mgc"
@@ -31,7 +31,7 @@ class Mgccli < Formula
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
         url "https://github.com/MagaluCloud/mgccli/releases/download/v0.38.0/mgccli_0.38.0_linux_amd64.tar.gz"
-        sha256 "2565ee0a4a0fc2f69b5c315a864b1afcf6dbc76dce86fe7724905598b69f9d8b"
+        sha256 "fb1dbb5c2b481019fded4a1e918fe424cc1c911bc6c51070d1ddfb14c854a9cc"
 
         def install
           bin.install "mgc"
@@ -41,7 +41,7 @@ class Mgccli < Formula
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
         url "https://github.com/MagaluCloud/mgccli/releases/download/v0.38.0/mgccli_0.38.0_linux_arm64.tar.gz"
-        sha256 "ed5fc74942eaf411c7069f20b15bf8b931892c1ef6d93512da6ab9e581a33c14"
+        sha256 "c7896fba4880890daa9c8639237d11b75b64ef845bd1e5a38b952627ace27856"
 
         def install
           bin.install "mgc"
